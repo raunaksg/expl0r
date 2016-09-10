ee3994d1fa028e982ebcc0c0df3a6e63ec592327
lindatxia <lindaxia@Lindas-MacBook-Pro.local>
2016-09-09 21:28:30 -0400

    first commit

--- Expl0r/ViewController.swift
+++ Expl0r/ViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBAction func AddressSelector(sender: UITextField) {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet var textField: UITextField!
+    
+    
+    
+    @IBAction func AddButton(sender: UIButton) {
+        
+        let newItem = textField.text
+        textField.keyboardType = UIKeyboardType.Default
+        textField.resignFirstResponder()
+        
+        print(newItem)
+        
+    }
+    
 }
 
